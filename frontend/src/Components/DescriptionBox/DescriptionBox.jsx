bb4ffbc3df4294a405439cd443872898c50d9d3f
jainmanan0296 <143293185+jainmanan0296@users.noreply.github.com>
2024-02-13 00:19:56 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Welcome to SHOPPER! Discover a world of convenience and choice at your fingertips. Browse through our curated selection of top-quality products, from fashion and electronics to home essentials and beyond. Enjoy seamless shopping, secure transactions, and swift deliveries right to your doorstep. With a user-friendly interface and a commitment to customer satisfaction, we strive to make your online shopping experience exceptional. Explore, shop, and indulge in the ease of finding everything you need, all in one place. Start your journey with us today!</p>
+        <p>Step into a universe of endless possibilities with SHOPPER. Dive into a diverse collection of premium products meticulously chosen to cater to your lifestyle needs. Whether it’s the latest in fashion trends, cutting-edge gadgets, or must-have home decor, we’ve got you covered. Our platform offers a seamless and secure shopping experience, ensuring your satisfaction from selection to delivery. Embrace the convenience of online shopping with our intuitive interface and reliable customer service. Join a community of savvy shoppers and elevate your shopping experience today!</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
