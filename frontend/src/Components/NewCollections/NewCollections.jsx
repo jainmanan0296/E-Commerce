52556c8b7d6484cee90a9779d40f0e658b6f703a
jainmanan0296 <143293185+jainmanan0296@users.noreply.github.com>
2024-07-02 23:05:10 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const[new_collection,setNew_collection] = useState([]);
   
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://e-commerce-backend-jrrq.onrender.com/newcollections')
     .then((res)=>res.json())
     .then((data)=>setNew_collection(data));
   },[]) 
